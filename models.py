9d65649dc6d702d24cbca5b29f0952bbc0d513ca
Elya_1011 <elya.alimova.01@mail.ru>
2025-12-04 15:33:53 +0300

    add class Base and SwapiPeople

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, MappedColumn, mapped_column
+from sqlalchemy import Integer, JSON
 from dotenv import load_dotenv
 import os
 
@@ -7,3 +9,20 @@ load_dotenv()
 PG_DNS = f'postgresql+asyncpg://{os.getenv('USER_DB')}:{os.getenv('PASSWORD_DB')}@localhost:5432/{os.getenv('NAME_DB')}'
 engine = create_async_engine(PG_DNS)
 DbSession = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+class Base(DeclarativeBase, AsyncAttrs):
+    pass
+
+
+class SwapiPeople(Base):
+    __tablename__ = "swapi_people"
+
+    id: MappedColumn[int] = mapped_column(Integer, primary_key=True)
+    json: MappedColumn[dict] = mapped_column(JSON)
+
+async def init_orm():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def close_orm():
+    await engine.dispose()
